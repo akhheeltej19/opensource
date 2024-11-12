b2dd60e16a65e8255e2d4acde258de10f93cb5a7
akhheeltej19 <akhheel7317@gmail.com>
2024-11-12 10:34:44 +0530

    Create Main.java

---
+++ eb_dsa/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main{
+  public static void main(string[] args){
+  Scanner sc = new Scanner(System.in);
+    int n = new sc.nextInt();
+    if(n%2==0){
+      System.out.println("Even number");
+    }else{
+      System.out.println("Odd number");
+    }
+  }
+}
+  
