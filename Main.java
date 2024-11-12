cd1646cca0a5e3f592c057b37ed56ad3a4abdc7f
akhheeltej19 <akhheel7317@gmail.com>
2024-11-12 10:28:01 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class main{
+  public static void main(string[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    if(n%2==0){
+      System.out.println("Even number");
+    }else{
+      System.out.println("Odd number");
+    }
+  }
+}
